d45ea0f3f35a27d7b6852d72a03f44828cf167a6
Yusuf J Shaikh <yusufsh23@gmail.com>
2017-04-18 13:14:01 -0700

    Initial Commit

---
+++ FinalProjectCS560/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FinalProjectCS560
+//
+//  Created by YusufShaikh on 4/18/17.
+//  Copyright (c) 2017 San Diego State University. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
